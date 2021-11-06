b149cde2578add329f515632d05091d4b05e180e
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-11-06 22:19:38 +0300

    add Perfect Numbers

---
+++ src/Homeworks/2ndDay/perfectNumbers.js
@@ -0,0 +1,21 @@
+function getTheDivisorsOfTheNumber(number) {
+    let divisorsOfNumbers = [];
+    for (let i = 1; i < number; i++) {
+        if (number % i == 0) {
+            divisorsOfNumbers.push(i);
+        }
+    }
+    return divisorsOfNumbers;
+}
+
+let perfectNumbers = [];
+for (let i = 1; i < 1000; i++) {
+    let divisorsOfTheNumber = getTheDivisorsOfTheNumber(i);
+    let total = divisorsOfTheNumber.reduce((acc, n) => acc + n, 0);
+
+    if (total == i) {
+        perfectNumbers.push(i);
+    }
+}
+
+console.log(perfectNumbers)
